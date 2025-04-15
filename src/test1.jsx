3059e99e9df8f0b117b7f53f8fa022e299b4242b
sayma ruhi <saymaruhi@outlook.com>
2025-04-15 15:37:43 -0400

    imported test1.jsx file to App.jsx file

--- src/test1.jsx
+++ src/test1.jsx
@@ -1,9 +1,10 @@
 function Test (){
     return(
         <div>
-            <h1>Sayma Ruhi</h1>
+            <h1>test1.jsx file. Below are the code</h1>
             <h2>GitHub</h2>
          // deleted h3
         </div>
     )
 }
+export default Test;
