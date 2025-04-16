298d3da65b60659fd3307d104e03cbfec212d1ff
sayma ruhi <saymaruhi@outlook.com>
2025-04-16 11:12:42 -0400

    added h2 in test1

--- src/test1.jsx
+++ src/test1.jsx
@@ -4,6 +4,7 @@ function Test (){
             <h1>test1.jsx file. Below are the code</h1>
             <h2 id= "fontclr"> My GitHub</h2>
          // Story1 Branch
+         <h2>working 2nd day </h2>
         </div>
     )
 }
