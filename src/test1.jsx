09f103779f1d3fb82240fc417d76c86ede36c56e
sayma ruhi <saymaruhi@outlook.com>
2025-04-16 11:04:41 -0400

    Index CSS changed font color

--- src/test1.jsx
+++ src/test1.jsx
@@ -2,7 +2,7 @@ function Test (){
     return(
         <div>
             <h1>test1.jsx file. Below are the code</h1>
-            <h2>GitHub</h2>
+            <h2 id= "fontclr"> My GitHub</h2>
          // Story1 Branch
         </div>
     )
